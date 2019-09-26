d21f2ad172371d7fff434b03962a1bf9a9ea0574
Rafael Rocha <rafael.rocha@quantilope.com>
2019-09-26 17:58:52 +0200

    add bcrypt, hash password and save salt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,7 @@ import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { InternalServerErrorException, ConflictException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,13 +10,20 @@ export class UserRepository extends Repository<User> {
     const { username, password } = authCredentialsDto;
 
     try {
-      await this.insert({ username, password });
+
+      const salt = await bcrypt.genSalt();
+      const saltedPassword = await this.hashPassword(password, salt);
+
+      await this.insert({ username, password: saltedPassword, salt });
     } catch ({ code, message }) {
       if (code === '23505') { // code for duplicate key
         throw new ConflictException(`There's already a user with username: ${username}`);
       }
-
       throw new InternalServerErrorException(message);
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
